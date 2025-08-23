6d101941c48e0cde952aff0ee9875d6af0e6cf7f
Gökhan Demirkıran <157534604+yapaygezegen@users.noreply.github.com>
2025-08-23 22:05:03 +0300

    wp.js

--- wp.js
+++ wp.js
@@ -1,4 +1,4 @@
-let dizi1 = ['k', '!', 'a', 't', 'n', 'e', 's', 'Ç', '?', 'ç', 'p', ';', 'S', 'w', '6', 'Q', 'ş', 'T', 'h', 'C', 'Ö', '.', 'İ', 'Ü', 'K', 'v', 'i', 'I', 'o', 'm', 'H', 'P', ',', 'd', 'ö', 'X', 'B', 'V', 'D', 'R', ':', 'G', 'l', 'y', 'r', 'A', 'M', 'Y', 'N', 'Ş', 'j', 'ğ', 'q', 'g', '7', '2', ' ', 'O', 'c', 'b', 'J', '8', 'Z', '0', 'U', 'ü', '4', 'E', 'L', 'x', '5', 'f', 'W', 'z', '3', '9', 'Ğ', 'F', 'ı', 'u', '1']
+let dizi1 = ['k', '!', 'a', 't', 'n', 'e', 's', 'Ç', '?', 'ç', 'p', ';', 'S', 'w', '6', 'Q', 'ş', 'T', 'h', 'C', 'Ö', '.', 'İ', 'Ü', 'K', 'v', 'i', 'I', 'o', 'm', 'H', 'P', ',', 'd', 'ö', 'X', 'B', 'V', 'D', 'R', ':', 'G', 'l', 'y', 'r', 'A', 'M', 'Y', 'N', 'Ş', 'j', 'ğ', 'q', 'g', '7', '2', ' ', 'O', 'c', 'b', 'J', '8', 'Z', '0', 'U', 'ü', '4', 'E', 'L', 'x', '5', 'f', 'W', 'z', '3', '9', 'Ğ', 'F', 'ı', 'u', '1',"+","-","/","&","|","é"]
 let dizi2 = dizi1.slice("").sort(() => Math.random() - 0.5)
 function degistir(veri, duzTers = 1) {
     let sonuc = "", a, b
@@ -10,7 +10,7 @@ function degistir(veri, duzTers = 1) {
         b = dizi1
     }
     for (let i = 0; i < veri.length; i++) {
-        for (let j = 0; j < 81; j++) {
+        for (let j = 0; j < dizi1.length; j++) {
             if (veri[i] == a[j]) {
                 sonuc += b[j]
             }
@@ -19,7 +19,7 @@ function degistir(veri, duzTers = 1) {
     return sonuc
 }
 document.addEventListener("click", function(a) {
-    if(a.srcElement.dataset.icon == "wds-ic-send-filled"){
+    if(a.srcElement.dataset.icon == "wds-ic-send-filled" || a.srcElement.ariaLabel == "Gönder"){
         document.querySelectorAll("p")[1].firstChild.firstChild.nodeValue = degistir(document.querySelectorAll("p")[1].firstChild.firstChild.nodeValue)
     }
 })
