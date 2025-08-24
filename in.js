0fea9afdb8ac6c580279e8a4cc03e802a0790019
Gökhan Demirkıran <157534604+yapaygezegen@users.noreply.github.com>
2025-08-24 21:27:18 +0300

    in.js

--- in.js
+++ in.js
@@ -1,6 +1,5 @@
-let dizi1 = ['r', 'm', 'H', 'v', 'K', 'i', 'n', 'Q', 'Ş', 'N', ':', 's', 'X', 'Z', 'ı', 'c', '3', 'Ğ', 'T', 'o', ';', 'C', 'V', 'Ç', '1', 'Y', 'I', 'ç', '!', 'y', 'ü', 'İ', 'D', 'g', '7', 'J', '.', 'x', 'u', '5', 'F', 'l', 'ş', '6', 'z', '4', 'q', 'j', 'U', 'G', 'k', 'Ü', 'd', '8', 'ö', 'e', '2', 'M', 'ğ', 'p', 'R', ',', 'a', 'E', 'S', 'h', 'W', '?', '9', 'b', 't', 'O', 'P', 'L', ' ', '0', 'A', 'w', 'Ö', 'B', 'f']
-let dizi2 = dizi1.slice().sort(() => Math.random() - 0.5)
-let acKapa = true
+let dizi1 = ['k', '!', 'a', 't', 'n', 'e', 's', 'Ç', '?', 'ç', 'p', ';', 'S', 'w', '6', 'Q', 'ş', 'T', 'h', 'C', 'Ö', '.', 'İ', 'Ü', 'K', 'v', 'i', 'I', 'o', 'm', 'H', 'P', ',', 'd', 'ö', 'X', 'B', 'V', 'D', 'R', ':', 'G', 'l', 'y', 'r', 'A', 'M', 'Y', 'N', 'Ş', 'j', 'ğ', 'q', 'g', '7', '2', ' ', 'O', 'c', 'b', 'J', '8', 'Z', '0', 'U', 'ü', '4', 'E', 'L', 'x', '5', 'f', 'W', 'z', '3', '9', 'Ğ', 'F', 'ı', 'u', '1',"+","-","/","&","|","é"]
+let dizi2 = dizi1.slice("").sort(() => Math.random() - 0.5)
 function degistir(veri, duzTers = 1) {
     let sonuc = "", a, b
     if (duzTers) {
@@ -11,31 +10,22 @@ function degistir(veri, duzTers = 1) {
         b = dizi1
     }
     for (let i = 0; i < veri.length; i++) {
-        for (let j = 0; j < 81; j++) {
+        for (let j = 0; j < dizi1.length; j++) {
             if (veri[i] == a[j]) {
                 sonuc += b[j]
+                break
             }
         }
     }
     return sonuc
 }
-document.addEventListener("mouseover", function(a) {
-    if((a.srcElement.innerText == "Gönder" || a.srcElement.innerText == "Send") && acKapa){
-        acKapa = false
+document.addEventListener("click", function(a) {
+    if(a.srcElement.innerText == "Gönder" || a.srcElement.innerText == "Send"){
         document.querySelectorAll("p")[0].firstChild.firstChild.nodeValue = degistir(document.querySelectorAll("p")[0].firstChild.firstChild.nodeValue)
     }
 })
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
-        acKapa = true
-    }
-})
 document.addEventListener("click", function(b) {
-    if((b.srcElement.innerText == "Gönder" || b.srcElement.innerText == "Send") && !acKapa){
-        acKapa = true
-    }
     if(b.srcElement.dir == "auto"){
         console.log(degistir(b.srcElement.innerText, 0))
     }
 })
-console.log(dizi2)
